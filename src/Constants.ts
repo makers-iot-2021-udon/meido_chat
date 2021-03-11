f307e0867060e2407693c8e68f36ada10b49056f
i15317 <30828280+notchman8600@users.noreply.github.com>
2021-03-11 16:43:44 +0900

    Liffアプリのプロジェクトの作り直し

---
+++ src/Constants.ts
@@ -0,0 +1,11 @@
+export const LANE_NUM: number = 6
+export const evilSpacing = (num: number): string => {
+    let str = ""
+    for (let i = 0; i < num; i++) {
+        str += " "
+    }
+    // console.log("hoge" + str + "piyo")
+    return str
+}
+export const myLiffId = process.env.REACT_APP_LIFF_ID || "";
+export const dummyIcon = '/dummyicon.svg';
